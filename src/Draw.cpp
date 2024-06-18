db6c0b99e6bcee034f1aeeaa58a09fcbd01c3c4e
SimonReese <59612841+SimonReese@users.noreply.github.com>
2024-06-19 00:07:17 +0200

    Raw implementation for plotting 4 lines of edges of the table with corners

--- src/Draw.cpp
+++ src/Draw.cpp
@@ -1,8 +1,172 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
 #include "Draw.h"
 
 
 cv::Mat Draw::detectTableCorners() const{
+    // 0. Smoothing
+    cv::Mat smoothed;
+    cv::medianBlur(this->currentFrame, smoothed, 5);
+    // 1. Filter table plane ---------------
+    // Convert to hsv
+    cv::Mat HSVframe;
+    cv::cvtColor(smoothed, HSVframe, cv::COLOR_BGR2HSV);
+
+    // Compute mean HSV value in the middle of image // Maybe is better to first find some lines, then compute dominat color and later filter the table more precisely?
+    int x_center, y_center;
+    x_center = this->currentFrame.cols / 2;
+    y_center = this->currentFrame.rows / 2;
+    int distance = 10;
+    double H, S, V, area = std::pow(2*distance, 2);
+    cv::Vec3b values;
+    for(int row = y_center - distance; row < y_center + distance; row++){
+        for (int col = x_center - distance; col < x_center + distance; col++){
+            values = HSVframe.at<cv::Vec3b>(cv::Point(col, row));
+            std::cout << "Reading values " << values << std::endl;
+            H += static_cast<double>(values[0]) / area;
+            S += static_cast<double>(values[1]) / area;
+            V += static_cast<double>(values[2]) / area;
+        }
+    }
+    // Define bounds for HSV
+    int H_bound, S_bound, V_bound;
+    H_bound = 20;
+    S_bound = 50;
+    V_bound = 130;
+    int H_lower_bound = H - H_bound, H_upper_bound = H + H_bound;
+    int S_lower_bound = S - S_bound, S_upper_bound = S + S_bound;
+    int V_lower_bound = V - V_bound, V_upper_bound = V + V_bound;
+
+    cv::Mat tableMask;
+    cv::inRange(HSVframe, cv::Scalar(H_lower_bound, S_lower_bound, V_lower_bound), cv::Scalar(H_upper_bound, S_upper_bound, V_upper_bound), tableMask);
     
+    // Clean the table mask
+    cv::Mat elem1 = cv::getStructuringElement(cv::MorphShapes::MORPH_RECT, cv::Size(25, 25));
+    cv::Mat elem2 = cv::getStructuringElement(cv::MorphShapes::MORPH_RECT, cv::Size(40, 40));
+    cv::morphologyEx(tableMask, tableMask, cv::MORPH_ERODE, elem1);
+    cv::morphologyEx(tableMask, tableMask, cv::MORPH_CLOSE, elem2);
+
+    // Find contours
+    std::vector<std::vector<cv::Point> > contours;
+    cv::findContours(tableMask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    // Find convex hull of those contours
+    // Unpack all points
+    std::vector<cv::Point> points;
+    for(std::vector<cv::Point> contour : contours){
+        for(cv::Point point : contour){
+            points.push_back(point);
+        }
+    }
+    // Find convex hull of table
+    std::vector<cv::Point> convexHull;
+    cv::convexHull(points, convexHull);
+    // Fill the convex hull
+    cv::fillConvexPoly(tableMask, convexHull, cv::Scalar(255));
+    
+    
+    
+    // NOT NEEDED - Just for showing field
+    // Make AND between table and mask
+    //cv::Mat maskedTable;
+    //cv::bitwise_and(this->currentFrame, this->currentFrame, maskedTable, tableMask);
+    // --------------------------
+
+    // 2. Find lines
+    cv::Mat edges;
+    cv::Canny(tableMask, edges, 100, 200);
+    
+    cv::Mat sides;
+    cv::cvtColor(edges, sides, cv::COLOR_GRAY2BGR);
+    std::vector<cv::Vec2f> lines;
+    cv::HoughLines(edges, lines, 1, CV_PI / 180, 100);
+    std::cout << lines.size() << std::endl;
+
+    // We need to filter lines
+    if (true){
+    // 1. Divide them by angle;
+    std::vector<cv::Vec2f> hLines, vLines;
+    float hMean = 0, vMean = 0;
+    for (cv::Vec2f line : lines){
+        float rho = line[0]; // Will be used to compute mean of rhos'
+        float theta = line[1];
+        std::cout << "Line theta " << theta << std::endl;
+        if (theta > CV_PI - (CV_PI/4 + CV_PI/6) || theta < (CV_PI / 4) + (CV_PI / 6)){
+            vLines.push_back(line);
+            vMean += rho;
+        }
+        else {
+            hLines.push_back(line);
+            hMean += rho;
+        }
+    }
+    std::cout << "Sizes " << vLines.size() << " " << hLines.size() << std::endl;
+    vMean = vMean / vLines.size();
+    hMean = hMean / hLines.size();
+
+    bool lower = false;
+    bool higher = false;
+    lines.clear(); // Clear all lines
+    // Now for each angle, we just need two lines
+    for (std::vector<cv::Vec2f>::iterator it = hLines.begin(); it != hLines.end(); ){
+        float rho = (*it)[0];
+        if ( !lower && rho < hMean) {
+            lines.push_back(*it);
+            lower = true;
+            it++;
+        }
+        else if (!higher){
+            lines.push_back(*it);
+            higher = true;
+            it++;
+        }
+        else {
+            it = hLines.erase(it);
+        }
+    }
+    higher = false;
+    lower = false;
+    for (std::vector<cv::Vec2f>::iterator it = vLines.begin(); it != vLines.end(); ){
+        float rho = (*it)[0];
+        if ( !lower && rho < vMean) {
+            lines.push_back(*it);
+            lower = true;
+            it++;
+        }
+        else if (!higher){
+            lines.push_back(*it);
+            higher = true;
+            it++;
+        }
+        else {
+            it = vLines.erase(it);
+        }
+    }
+    }
+    // At this point we should have only 4 lines
+
+    // Draw lines
+    for (int i = 0; i < lines.size(); i++){
+        float rho = lines[i][0], theta = lines[i][1];
+        std::cout << "Filetred theta" << theta << std::endl;
+        cv::Point start, end;
+        start.x = std::round(rho * std::cos(theta) - 1000*std::sin(theta));
+        start.y = std::round(rho * std::sin(theta) + 1000*std::cos(theta));
+
+        end.x = std::round(rho * std::cos(theta) + 1000*std::sin(theta));
+        end.y = std::round(rho * std::sin(theta) - 1000*std::cos(theta));
+
+        cv::line(sides, start, end, cv::Scalar(0, 0, 255), 2);
+    }
+
+    return sides;
+    
+    // 3. Find corners
+    cv::Mat corners;
+    cv::cvtColor(sides, sides, cv::COLOR_BGR2GRAY);
+    cv::cornerHarris(sides, corners, 10, 5, 0.5);
+    return tableMask;
 }
 
 Draw::Draw(std::string fieldPath)
@@ -15,5 +179,5 @@ void Draw::setCurrentFrame(const cv::Mat &currentFrame){
 
 void Draw::getGameDraw(cv::Mat &outputDrawing) const{
     // Currently just return the current frame
-    this->currentFrame.copyTo(outputDrawing);
+    outputDrawing = detectTableCorners();
 }
