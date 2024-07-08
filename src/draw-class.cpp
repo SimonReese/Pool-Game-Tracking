2bb55d6e0c34df769a586860ea00a1f6d0861caf
SimonReese <59612841+SimonReese@users.noreply.github.com>
2024-07-08 02:17:18 +0200

    Added code to test new functions

--- src/draw-class.cpp
+++ src/draw-class.cpp
@@ -7,6 +7,8 @@
 #include <opencv2/highgui.hpp>
 #include "Draw.h"
 
+#include <opencv2/imgproc.hpp>
+
 int main(int argc, char* argv[]){
     if (argc < 2){
         std::cerr << "Please provide a video or an image path." << std::endl;
@@ -24,7 +26,16 @@ int main(int argc, char* argv[]){
     // Get drawing
     cv::Mat result;
     drawing.getGameDraw(result);
+    cv::imshow("Correction", result);
+    cv::imshow("Original", frame);
 
+    // Testing overlapping function
+    cv::Mat spiderMan = cv::imread("../res/spider-man-3.png");
+    cv::Mat testImage = cv::imread("../res/opencv-test-image.png");
+    cv::resize(spiderMan, spiderMan, cv::Size(300, 300));
+    cv::Mat over = drawing.drawOver(testImage, spiderMan, cv::Point(testImage.cols / 2, testImage.rows / 2));
+    cv::imshow("Overlap", over);
+    //animation();
     cv::waitKey(0);
     return 0;
 }
