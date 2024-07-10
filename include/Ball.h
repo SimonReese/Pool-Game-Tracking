72ec5254aa0bed3b3b1bda1a906202c4a6b9b48f
Alebozz <ale.bozzon4@gmail.com>
2024-07-10 14:40:00 +0200

    added comments to files

--- include/Ball.h
+++ include/Ball.h
@@ -24,18 +24,44 @@ private:
 
 public:
 
+    /**
+     * Default constructor for initializing class.
+     * 
+     * @param circle_radius_and_center center point and radius of the circle representing the ball
+     */
     Ball(cv::Vec3f circle_radius_and_center);
 
+    /**
+     * Set the class of the ball
+     * @param ball_class class to be setted.
+     */
     void setBallClass(int ball_class);
 
+    /**
+     * Return the class of the ball
+     */
     int getBallClass();
 
+    /**
+     * Set the bounding box enclosing the ball
+     * @param bounding_box parameters of the bounding box enclosing the ball
+     */
     void setBoundingBox(cv::Rect bounding_box);
 
+    /**
+     * Return the bounding box of the ball
+     */
     cv::Rect getBoundingBox();
 
+    /**
+     * Set the ball center and radius
+     * @param circle_radius_and_center parameters of the circle corresponding to the ball
+     */
     void setBallPosition(cv::Vec3f circle_radius_and_center);
 
+    /**
+     * Return the center and radius of the ball
+     */
     cv::Vec3f getBallPosition();
 
 };
--- include/BallsDetection.h
+++ include/BallsDetection.h
@@ -12,15 +12,40 @@
 #include "FieldGeometryAndMask.h"
 #include "Ball.h"
 
+    /**
+     * Return a vector containing all the ball found on the field
+     * @param only_table_image image where to look for the balls
+     * @param field_contour mask of the field used to remove false positive balls inside and outside the playing field
+     * @param boundaries_contours_poly polygon representing the contour of the playing field, used fo the removal of false positive balls inside the playing field
+     * @param sorted_corners 4 corners of the playing field, used for the removal of false positive ball close to the corners of the field
+     */
+    std::vector<Ball> findBalls(const cv::Mat only_table_image, const cv::Mat field_contour, std::vector<cv::Point> boundaries_contours_poly, std::vector<cv::Point2i> sorted_corners);
 
-std::vector<Ball> findBalls(const cv::Mat only_table_image, const cv::Mat field_contour, std::vector<cv::Point> boundaries_contours_poly, std::vector<cv::Point2i> sorted_corners);
+    /**
+     * Modify the image by drawing the circles corresponding to the balls
+     * @param balls vector of balls from which circles position and radius are retrieved from
+     * @param image input image where the circles will be drawn
+     */
+    void drawBallsHSVChannels(std::vector<Ball> balls, cv::Mat &image);
 
-void drawBallsHSVChannels(std::vector<Ball> balls, cv::Mat &image);
+    /**
+     * Modify the image by drawing the boudning box around found balls
+     * @param balls vector of balls from which bounding box parameters are retrieved from
+     * @param image input image where the bounding boxes will be drawn
+     */
+    void drawBoundingBoxesHSVChannels(std::vector<Ball> balls, cv::Mat &image);
 
-void drawBoundingBoxesHSVChannels(std::vector<Ball> balls, cv::Mat &image);
+    /**
+     * Write bounding box and class of every ball found for future calculation of evaluation metrics
+     * @param bboxFileName filename of the file where to write the parameters of the bounding boxes
+     * @param balls vector of balls from which bounding box parameters and class value are retrieved from
+     */
+    void writeBboxToFile(std::string bboxFileName,std::vector<Ball> balls); //when conmplete it will print both bounding box and class of every ball
 
-void writeBboxToFile(std::string bboxFileName,std::vector<Ball> balls); //when conmplete it will print both bounding box and class of every ball
-
-std::vector<cv::Rect> findBoundingRectangles(const cv::Mat field_mask_and_balls);
+    /**
+     * Return a vector containing all the bounding boxes found, one for each ball detected in the previous functions
+     * @param field_mask_and_balls image containing the circles that identifies the balls
+     */
+    std::vector<cv::Rect> findBoundingRectangles(const cv::Mat field_mask_and_balls);
 
 #endif
