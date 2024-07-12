b16c31b4d6178b7a2cf24c7dfa0e93cad1b0624f
SimonReese <59612841+SimonReese@users.noreply.github.com>
2024-07-12 19:35:25 +0200

    Implement mask functionality

--- include/TableSegmenter.h
+++ include/TableSegmenter.h
@@ -1,5 +1,5 @@
 /**
- * @author Simone Peraro
+ * @author .
  * TODO: who will maintain this file?
  */
 
@@ -14,8 +14,24 @@ class TableSegmenter{
 
     private:
 
+    /**
+     * Return a vector containing the mean color for each of the 3 channels (mainly used with HSV color space) with the kernel centered on the center of the image
+     * @param image image where to compute the mean of the color
+     * @param kernel_size size of the square kernel used. height = width = kernel_size
+     */    
+    cv::Vec3b fieldMeanColor(const cv::Mat& image, int kernel_size) const;
+
+    /**
+     * Return a cv::Mat containing the mask of the field without considering the balls
+     * @param image image where to compute the mask from
+     * @param mean_color vector containing the mean color to use for the selection of pixels that will form the mask
+     */ 
+    cv::Mat computeFieldMask(const cv::Mat image, cv::Vec3b mean_color) const;
+
 
     public:
+
+    cv::Mat getTableMask(const cv::Mat& frame) const;
 
     /**
      * Returns the playing field corners
@@ -23,7 +39,6 @@ class TableSegmenter{
      * Returns a vector containing the 4 points identifying the 4 corners of the 
      * playing field sorted as: top_left, top_right, bottom_right, bottom_left
      * 
-     * @param approximate_field_lines
      */
     std::vector<cv::Point2i> findFieldCorners();
 
--- src/TableSegmenter.cpp
+++ src/TableSegmenter.cpp
@@ -1,13 +1,59 @@
 /**
- * @author Simone Peraro.
+ * @author .
  * TODO: who will maintain this file?
  */
 
 #include "TableSegmenter.h"
 
+#include <opencv2/imgproc.hpp>
 
+cv::Vec3b TableSegmenter::fieldMeanColor(const cv::Mat& image, int kernel_size) const{
+
+    int x = image.size().width/2; /*defines the coordinates of the center of the image*/
+    int y = image.size().height/2;
+
+     std::vector<cv::Vec3b> vec; /*retrieve the values of all the pixels inside the kernel window and stores them in a vector*/
+        for (int i = y-kernel_size/2; i <= y+kernel_size/2 && i < image.size().height; i++)
+        {
+            for (int j = x-kernel_size/2; j <= x+kernel_size/2 && j < image.size().width; j++)
+            {
+                if(i < 0 || j < 0){
+                    continue;
+                }else{
+                    vec.push_back(image.at<cv::Vec3b>(i,j));
+                }
+            }
+            
+        }
+        
+
+        /*evaluates average value for h,s,v channels*/
+        uint32_t h = 0;
+        uint32_t s = 0;
+        uint32_t v = 0;
+        uchar k = 0;
+        for (k; k < vec.size(); k++)
+        {
+            h = h + (uint32_t)(vec[k].val[0]);
+            s = s + (uint32_t)(vec[k].val[1]);
+            v = v + (uint32_t)(vec[k].val[2]); 
+        }
+    cv::Vec3b mean_color(h/k,s/k,v/k); /*stores the average value of the 3 channels in a vector*/
+    return mean_color;
+
+}
+
+cv::Mat TableSegmenter::getTableMask(const cv::Mat &frame) const{
+
+    cv::Mat blurred;
+    cv::GaussianBlur(frame,blurred,cv::Size(7,7),0,0); // used to find field mask
+    cv::Vec3b mean_color = fieldMeanColor(blurred,11);
+    cv::Mat filled_field_contour = computeFieldMask(blurred,mean_color);
+    
+    return filled_field_contour;
+}
 
 std::vector<cv::Point2i> TableSegmenter::findFieldCorners(){
-    
-return std::vector<cv::Point2i>();
+
+    return std::vector<cv::Point2i>();
 }
